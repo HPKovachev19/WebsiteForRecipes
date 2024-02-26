38d75f9c30b52f0ad317136115df5f4b8304f8e1
HPKovachev19 <HPKovachev19@codingburgas.bg>
2024-02-26 22:29:38 +0200

    Implement login and register

---
+++ recipeWebsite/recipeWebsite/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace recipeWebsite.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
